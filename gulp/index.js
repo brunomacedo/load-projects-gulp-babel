e94ae75ec14a755ae03905a319f86df11768e3f4
Bruno Macedo <contato@brunomacedo.com.br>
2018-05-25 18:26:29 -0300

    Remove logs

--- gulp/index.js
+++ gulp/index.js
@@ -2,9 +2,6 @@ import gulp from 'gulp';
 import promptProjects from './prompt';
 
 gulp.task('open', () => {
-	// console.log(good('Arg name: ', args.name));
-
-	// console.log(warning('trainlingSlash: '), error(getCurrent.trainlingSlash()));
 	promptProjects();
 });
 
