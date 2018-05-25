479a63b44fcc8dfd1cc75b12b166cd716285ff41
Bruno Macedo <contato@brunomacedo.com.br>
2018-05-25 18:00:04 -0300

    Module Gulp

---
+++ gulp/logs.js
@@ -0,0 +1,7 @@
+const utilC = require('gulp-util');
+
+const error = utilC.colors.bold.red;
+const warning = utilC.colors.bold.yellow;
+const good = utilC.colors.bold.green;
+
+module.exports = { error, warning, good };
