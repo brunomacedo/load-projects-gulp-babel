8d031407283c5573353864e38cbc9d2b98a035a9
Bruno Macedo <contato@brunomacedo.com.br>
2018-05-28 10:36:10 -0300

    [GULP] Test callback dest

--- gulp/index.js
+++ gulp/index.js
@@ -1,4 +1,9 @@
 import gulp from 'gulp';
+import sass from 'gulp-sass';
+import compass from 'gulp-compass';
+import autoprefixer from 'gulp-autoprefixer';
+import path from 'path';
+
 import { promptProjects, newObj } from './prompt';
 import { error, warning, good } from './logs';
 
@@ -11,4 +16,31 @@ gulp.task('open', () => {
 
 gulp.task('close', () => {
 	console.log(warning('Assets: ', error(newObj.assets)));
+});
+
+
+// START
+
+gulp.task('sass', () => {
+	gulp.src('assets/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({browsers: ['last 40 versions', '> 5%', 'Firefox ESR']}))
+		.pipe(gulp.dest((element) => {
+			console.log('DIRNAME PATH GULP >>>>>>>>>> ', element.dirname);
+		}));
+});
+
+gulp.task('compass', () => {
+	gulp.src('assets/**/*.scss')
+		.pipe(compass({
+			config_file: './config.rb',
+			css: 'assets',
+			sass: 'assets'
+		}))
+		.pipe(autoprefixer({browsers: ['last 40 versions', '> 5%', 'Firefox ESR']}))
+		.pipe(gulp.dest('./assets/'));
+});
+
+gulp.task('default', () => {
+	gulp.watch('assets/**/*.scss', ['sass']);
 });
